27596d4ee52a525592da3e175c7325b0c4cf57a6
Dhruv-75 <130495046+Dhruv-75@users.noreply.github.com>
2024-07-11 16:35:43 -0400

    changes on thumbnail

--- src/Component/VideoSugg/videosugg.js
+++ src/Component/VideoSugg/videosugg.js
@@ -1,31 +1,33 @@
 import React from "react";
 import { useNavigate } from 'react-router-dom';
 import './videosugg.css';
+import spriteImage from '/videos/css_sprites.png'; // Adjust path as necessary
 
 function Videosugg() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const videos = [
-        { id: 1, thumbnail: process.env.PUBLIC_URL + '/konubhai.jpg', videoUrl: '/videos/Assignment2.mp4' },
-        { id: 2, thumbnail: process.env.PUBLIC_URL + '/konubhai.jpg', videoUrl: '/videos/Assignment2.mp4' },
-        { id: 2, thumbnail: process.env.PUBLIC_URL + '/konubhai.jpg', videoUrl: '/videos/Assignment2.mp4' },
-        { id: 2, thumbnail: process.env.PUBLIC_URL + '/konubhai.jpg', videoUrl: '/videos/Assignment2.mp4' },
-        { id: 2, thumbnail: process.env.PUBLIC_URL + '/konubhai.jpg', videoUrl: '/videos/Assignment2.mp4' },
-      
-      ];
-  
-      const handleImageClick = (videoUrl) => {
-        navigate(`/videoplayer?video=${encodeURIComponent(videoUrl)}`);
-      };
-    
-      return (
-        <div className="container">
-          {videos.map((video) => (
-            <div className="video-suggestion" key={video.id} onClick={() => handleImageClick(video.videoUrl)}>
-              <img src={video.thumbnail} alt="Thumbnail" className="video-logo" />
-            </div>
-          ))}
-        </div>     );
-    }
-    
+  const videos = [
+    { id: 1, thumbnail: process.env.PUBLIC_URL + '/konubhai.jpg', sprite: spriteImage, videoUrl: '/videos/Assignment2.mp4' },
+    { id: 2, thumbnail: process.env.PUBLIC_URL + '/konubhai.jpg', sprite: spriteImage, videoUrl: '/videos/Assignment2.mp4' },
+    { id: 3, thumbnail: process.env.PUBLIC_URL + '/konubhai.jpg', sprite: spriteImage, videoUrl: '/videos/Assignment2.mp4' },
+    { id: 4, thumbnail: process.env.PUBLIC_URL + '/konubhai.jpg', sprite: spriteImage, videoUrl: '/videos/Assignment2.mp4' },
+    { id: 5, thumbnail: process.env.PUBLIC_URL + '/konubhai.jpg', sprite: spriteImage, videoUrl: '/videos/Assignment2.mp4' },
+  ];
+
+  const handleImageClick = (videoUrl) => {
+    navigate(`/videoplayer?video=${encodeURIComponent(videoUrl)}`);
+  };
+
+  return (
+    <div className="container">
+      {videos.map((video) => (
+        <div className="video-suggestion" key={video.id} onClick={() => handleImageClick(video.videoUrl)}>
+          <img src={video.thumbnail} alt="Thumbnail" className="video-logo" />
+          <div className="sprite-preview" style={{ backgroundImage: `url(${video.sprite})` }}></div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
 export default Videosugg;
